1d183d98f36c247599142dc15242cdeca9d8114e
Pablo Riveiro <58869893+TheWhiteMachine@users.noreply.github.com>
2020-05-29 02:02:03 -0300

    primers
    primeros

---
+++ codigo.js
@@ -0,0 +1,8 @@
+var app = angular.module("appa", []);
+app.controller("controlador", function($scope){
+ $scope.nombre="";
+ $scope.datos="";
+ var opciones =[{raza:"chiguagua",raza:"beduino"}];
+ $scope.razas=opciones;
+console.log(razas);
+});

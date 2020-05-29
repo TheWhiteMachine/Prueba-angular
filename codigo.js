8ba18683b4972a0f4a458bfa3bcb573db4067bfb
Nach <ignacio.talavera@gmail.com>
2020-05-29 18:02:40 -0300

    Se corrigieron errores de dislexia

--- codigo.js
+++ codigo.js
@@ -1,8 +1,12 @@
 var app = angular.module("appa", []);
+
 app.controller("controlador", function($scope){
  $scope.nombre="";
  $scope.datos="";
- var opciones =[{raza:"chiguagua",raza:"beduino"}];
+ var opciones =[{raza:"chiguagua"},{raza:"beduino"}];
  $scope.razas=opciones;
-console.log(razas);
+ $scope.razaSeleccionada={};
+console.log($scope.razas);
+
+
 });

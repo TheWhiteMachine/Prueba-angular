60f79a9f4aed24f1b68f7885d4415841394ca03d
TheWhiteMachine <priveiro@gmail.com>
2020-05-30 00:15:17 -0300

    estilo

--- codigo.js
+++ codigo.js
@@ -3,10 +3,24 @@ var app = angular.module("appa", []);
 app.controller("controlador", function($scope){
  $scope.nombre="";
  $scope.datos="";
- var opciones =[{raza:"chiguagua"},{raza:"beduino"}];
+ var opciones =[{raza:"Chiguagua"},{raza:"Barbilla"},{raza:"Doberman"},{raza:"Salchicha"},{raza:"Labrador"}];
  $scope.razas=opciones;
+var extremidades =[1,2,4,5];
+$scope.patasSeleccionadas=[];
+ $scope.patas= extremidades;
  $scope.razaSeleccionada={};
 console.log($scope.razas);
+$scope.boton= {
+    "background-color":"green",
+    "color":"white",
+    "margin-left":"20px",
+    "margin-top":"15px",
+    "marging-down":"15px",
+    "margin-right":"20px"
+}
+var quitar =function(){
+    seve=true;
+}
 
 
 });
